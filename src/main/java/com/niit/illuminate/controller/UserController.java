ead125e1416b0fef9664c81c37aa9c11534d9fa3
unknown <mohnish.karhade@gmail.com>
2017-03-07 15:01:11 +0530

    integrate with backend project

--- src/main/java/com/niit/illuminate/controller/UserController.java
+++ src/main/java/com/niit/illuminate/controller/UserController.java
@@ -1,43 +1,28 @@
 package com.niit.illuminate.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.niit.illuminatebe.model.Users;
-import com.niit.illuminatebe.service.UsersService;
 
 @Controller
 public class UserController {
-	
-//	@Autowired
-//	private UsersService usersService;
-//
-//	@RequestMapping("/login")
-//	public ModelAndView getLogin() {
-//
-//		ModelAndView model = new ModelAndView("login");
-//		model.addObject("msg", "User login");
-//
-//		return model;
-//
-//	}
-//
-//	@RequestMapping("/register")
-//	public String viewRegister(Model model) {
-//
-//		Users users = new Users();
-//		model.addAttribute("users", users);
-//		return "register";
-//	}
-//
+		
+
+	@RequestMapping("/login")
+	public ModelAndView getLogin() {
+
+		ModelAndView model = new ModelAndView("login");
+		model.addObject("msg", "User login");
+
+		return model;
+
+	}
+
+	@RequestMapping("/register")
+	public String viewRegister() {		
+		return "register";
+	}
+
 //	@RequestMapping(value = "/register", method = RequestMethod.POST)
 //	public String saveUser(@ModelAttribute("users") Users users, BindingResult result, Model model) {
 //		
